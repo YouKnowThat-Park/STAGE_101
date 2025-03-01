b03190a0fc4fff4ea8a0de09ceb06d557252f5ec
wooseok <youkn0wthat@naver.com>
2025-03-02 01:29:06 +0900

    🔥 yarn 삭제하고 npm 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    domains: ['zxryfhnxexxyxmrctocl.supabase.co'], // ✅ Supabase 이미지 도메인 추가
+  },
+};
 export default nextConfig;
