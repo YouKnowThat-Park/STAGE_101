873f870832d4da3f3cd576e81699871b705043cc
wooseok <youkn0wthat@naver.com>
2025-03-02 01:31:03 +0900

    ✨ Cart API 로직

---
+++ src/utils/api/fetchCartData.ts
@@ -0,0 +1,13 @@
+import { serverSupabase } from '@/supabase/supabase-server';
+import { CartType } from '@/types/cart.type';
+
+const fetchCartData = async (userId: string): Promise<CartType[]> => {
+  const supabase = serverSupabase();
+
+  const { data, error } = await supabase.from('cart').select('*').eq('user_id', userId);
+  if (error) throw new Error(error.message);
+
+  return data;
+};
+
+export default fetchCartData;
