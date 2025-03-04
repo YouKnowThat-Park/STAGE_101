112646f1c9fc94138ff07efbed55dcd1774b61be
wooseok <youkn0wthat@naver.com>
2025-03-04 22:46:03 +0900

    💄 Mypage 버튼 디자인 수정

--- src/app/mypage/_components/MypageButton.tsx
+++ src/app/mypage/_components/MypageButton.tsx
@@ -1,27 +1,21 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 interface MypageButtonProps {
   onClick: () => void;
-  label: string;
+  label: string | ReactNode;
   isActive?: boolean;
   variant?: 'primary' | 'secondary';
 }
-
 const MypageButton = ({ onClick, isActive, variant = 'primary', label }: MypageButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-2 rounded transition ${
-        isActive
-          ? 'bg-blue-500 text-white'
-          : variant === 'primary'
-            ? 'bg-white '
-            : 'bg-red-500 text-white hover:bg-red-600'
+      className={`px-3 py-2 rounded transition-all duration-200 transform active:scale-105 ${
+        variant === 'primary' ? ' text-black' : 'bg-red-500 text-white hover:bg-red-600'
       }`}
     >
       {label}
     </button>
   );
 };
-
 export default MypageButton;
