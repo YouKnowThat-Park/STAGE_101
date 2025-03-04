6bfb44f8eba0f18fc63d8d121eb9d37ce360c804
wooseok <youkn0wthat@naver.com>
2025-03-04 22:44:08 +0900

    💄 Mypage 디자인 수정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,21 +1,57 @@
 'use client';
 import { useUserStore } from '@/store/userStore';
 import React, { useState } from 'react';
-import MypageCart from './_components/MypageCart';
 import MypageHistory from './_components/MypageHistory';
 import MypageQna from './_components/MypageQna';
 import MypageTicket from './_components/MypageTicket';
 import MypageButton from './_components/MypageButton';
 import MypageProfile from './_components/MypageProfile';
 import { useUserHook } from '@/hooks/useUserHook';
+import TicketIcon from '@/ui/icon/TicketIcon';
+import InquiryIcon from '@/ui/icon/InquiryIcon';
+import MypageReview from './_components/MypageReview';
+import ReviewIcon from '@/ui/icon/ReviewIcon';
+import HistoryIcon from '@/ui/icon/HistoryIcon';
 
 const defaultProfileImg = '/next.svg'; // ✅ public 폴더 이미지 경로
 
 const buttonTabs = [
-  { key: 'ticket', label: '티켓' },
-  { key: 'cart', label: '리뷰' },
-  { key: 'history', label: '거래내역' },
-  { key: 'qna', label: '문의내역' },
+  {
+    key: 'ticket',
+    label: (
+      <div className="flex flex-col items-center">
+        <TicketIcon />
+        <span>TICKET</span>
+      </div>
+    ),
+  },
+  {
+    key: 'review',
+    label: (
+      <div className="flex flex-col items-center">
+        <ReviewIcon />
+        <span>REVIEW</span>
+      </div>
+    ),
+  },
+  {
+    key: 'history',
+    label: (
+      <div className="flex flex-col items-center">
+        <HistoryIcon />
+        <span>HISTORY</span>
+      </div>
+    ),
+  },
+  {
+    key: 'qna',
+    label: (
+      <div className="flex flex-col items-center">
+        <InquiryIcon />
+        <span>Q&A</span>
+      </div>
+    ),
+  },
 ];
 
 const MyPage = () => {
@@ -26,7 +62,7 @@ const MyPage = () => {
   console.log('data', name, point);
 
   return (
-    <div className="w-[600px] h-full flex flex-col justify-end items-center  p-6">
+    <div className="w-[600px] h-full flex flex-col justify-end ml-auto mr-20">
       {/* 프로필 & 유저 정보 */}
       <MypageProfile
         profile_img={profile_img || defaultProfileImg}
@@ -36,8 +72,8 @@ const MyPage = () => {
       />
 
       {/* 네비게이션 탭 */}
-      <div className="bg-slate-300 max-w-4xl p-6 ">
-        <nav className="flex justify-center items-center gap-20">
+      <div className="bg-white max-w-4xl p-2 ">
+        <nav className="flex justify-center items-center gap-20 border-b-2">
           {buttonTabs.map((tab) => (
             <MypageButton
               key={tab.key}
@@ -47,12 +83,11 @@ const MyPage = () => {
             />
           ))}
         </nav>
-
         {/* 선택된 탭에 따라 내용 변경 */}
-        <div className="mt-6">
+        <div className="mt-6 ">
           {selectedTab === 'ticket' && <MypageTicket />}
 
-          {selectedTab === 'cart' && <MypageCart />}
+          {selectedTab === 'review' && <MypageReview />}
 
           {selectedTab === 'history' && <MypageHistory />}
 
