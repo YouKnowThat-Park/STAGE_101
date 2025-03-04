b090e3039c1154fae365f6fd3b0a5f22c9db2165
wooseok <youkn0wthat@naver.com>
2025-03-04 22:35:07 +0900

    ✨ 리뷰 아이콘

---
+++ src/ui/icon/ReviewIcon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ReviewIcon = () => {
+  return (
+    <svg height="32" width="32" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M0 0h48v48H0zM30.71 12.83l-3.54-3.54c-.39-.39-1.02-.39-1.41 0L12 23.05v4.95h4.95l13.76-13.76c.39-.39.39-1.02 0-1.41z"
+        fill="none"
+      />
+      <path
+        fill="#8BC4FF"
+        d="M40 4H8c-2.21 0-3.98 1.79-3.98 4L4 44l8-8h28c2.21 0 4-1.79 4-4V8c0-2.21-1.79-4-4-4zm-28 24v-4.95L25.76 9.29c.39-.39 1.02-.39 1.41 0l3.54 3.54c.39.39.39 1.02 0 1.41L17.95 28h-4.95zm24 0H17l4-4h15v4z"
+      />
+    </svg>
+  );
+};
+
+export default ReviewIcon;
