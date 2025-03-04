89d90a10528f74bbd836dd015fb5b033e7c6364f
wooseok <youkn0wthat@naver.com>
2025-03-04 22:40:52 +0900

    ✨ Qna Tanstack Query 로직

---
+++ src/hooks/useQnaData.ts
@@ -0,0 +1,10 @@
+import fetchQnaData from '@/utils/api/fetchQnaData';
+import { useQuery } from '@tanstack/react-query';
+
+export const useQnaData = () => {
+  return useQuery({
+    queryKey: ['qna'],
+    queryFn: fetchQnaData,
+    staleTime: 1000 * 60 * 5,
+  });
+};
