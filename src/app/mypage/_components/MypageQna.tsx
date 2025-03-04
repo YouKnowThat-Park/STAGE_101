400b80ab6f860769690dfe9d5ae71b64ae28c45d
wooseok <youkn0wthat@naver.com>
2025-03-04 22:42:23 +0900

    ✨ Mypage Qna 로직

--- src/app/mypage/_components/MypageQna.tsx
+++ src/app/mypage/_components/MypageQna.tsx
@@ -1,10 +1,30 @@
+import { useQnaData } from '@/hooks/useQnaData';
+import NoQnAIcon from '@/ui/icon/NoQnAIcon';
 import React from 'react';
 
 const MypageQna = () => {
+  const { data: qna } = useQnaData();
+
+  const formatDateToKST = (dateString: string) =>
+    new Date(dateString).toLocaleDateString('ko-KR', { timeZone: 'Asia/Seoul' });
   return (
-    <section className="bg-white p-4 rounded shadow">
-      <h2 className="text-lg font-bold mb-4">문의 내역</h2>
-      <p>문의한 내역을 확인할 수 있습니다.</p>
+    <section className="bg-white flex flex-col">
+      <ul>
+        {qna?.length > 0 ? (
+          qna.map((qna: any) => (
+            <li key={qna.id}>
+              <h2 className="font-medium text-xl">{qna.title}</h2>
+              <p>{formatDateToKST(qna.created_at)}</p>
+              <p className="text-base">{qna.message}</p>
+            </li>
+          ))
+        ) : (
+          <div className="flex flex-col justify-center items-center">
+            <NoQnAIcon />
+            <p>문의한 내역이 없습니다.</p>
+          </div>
+        )}
+      </ul>
     </section>
   );
 };
