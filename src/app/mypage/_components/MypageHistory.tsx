c5f45ea11c9c84884306f7234a3b987b8d256c31
wooseok <youkn0wthat@naver.com>
2025-03-04 22:46:30 +0900

    ✨ Mypage History 컴포넌트 생성

--- src/app/mypage/_components/MypageHistory.tsx
+++ src/app/mypage/_components/MypageHistory.tsx
@@ -1,10 +1,11 @@
+import NoHistoryIcon from '@/ui/icon/NoHistoryIcon';
 import React from 'react';
 
 const MypageHistory = () => {
   return (
-    <section className="bg-white p-4 rounded shadow">
-      <h2 className="text-lg font-bold mb-4">거래 내역</h2>
-      <p>여기에 과거 거래 내역을 표시할 수 있습니다.</p>
+    <section className="bg-white flex flex-col justify-center items-center gap-5">
+      <NoHistoryIcon />
+      <p>거래 내역이 없습니다.</p>
     </section>
   );
 };
