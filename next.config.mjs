d9a1b4ab22117beb615d408b22f5784990c317b8
wooseok <youkn0wthat@naver.com>
2025-03-12 14:09:42 +0900

    📝 next.config에 domain 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['zxryfhnxexxyxmrctocl.supabase.co'], // ✅ Supabase 이미지 도메인 추가
+    domains: [
+      'zxryfhnxexxyxmrctocl.supabase.co', // ✅ Supabase 이미지 도메인
+      'k.kakaocdn.net', // ✅ 카카오 프로필 이미지 도메인 추가
+      'lh3.googleusercontent.com', // ✅ Google 프로필 이미지 도메인 추가
+    ],
   },
 };
+
 export default nextConfig;
