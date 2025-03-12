e2b1778f03e08b037bfdab0d6b9d2e5773eceb62
wooseok <youkn0wthat@naver.com>
2025-03-12 14:34:40 +0900

    ✨ + -  SVG 아이콘 생성

---
+++ src/ui/icon/PlusIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const PlusIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 32"
+      width={20}
+      height={20}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="16" x2="16" y1="7" y2="25" />
+      <line x1="7" x2="25" y1="16" y2="16" />
+    </svg>
+  );
+};
+
+export default PlusIcon;
