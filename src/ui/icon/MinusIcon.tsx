e16f50f220f49f07e8615d50d79b263b6e052d31
wooseok <youkn0wthat@naver.com>
2025-03-12 14:37:11 +0900

    ✨ - SVG 아이콘;;; 생성

---
+++ src/ui/icon/MinusIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MinusIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      width={20}
+      height={20}
+      fill="black"
+    >
+      <path d="M384,265H128v-17h256V265z" />
+    </svg>
+  );
+};
+
+export default MinusIcon;
