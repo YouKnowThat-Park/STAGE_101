1612abffb6479660e9c9a9f287584d329e12d86a
wooseok <youkn0wthat@naver.com>
2025-03-14 01:04:16 +0900

    📝 링크 주소 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <p>Welcome to Home!</p>
       </div>
       <div className="flex gap-10">
-        <Link href={'/guestbook'}>
+        <Link href={'/reviews'}>
           <div className=" h-[400px] bg-white shadow-md rounded-lg mt-20">
             <p>방명록 남기러 가기</p>
           </div>
