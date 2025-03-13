cd9e0f18cb550d21d93ce208559ba5e527664940
wooseok <youkn0wthat@naver.com>
2025-03-14 01:07:04 +0900

    💄 QNA 디자인 수정 (QNA가 아닌 FAQ 로 수정해야됨)

--- src/app/mypage/_components/MypageQna.tsx
+++ src/app/mypage/_components/MypageQna.tsx
@@ -7,24 +7,28 @@ const MypageQna = () => {
 
   const formatDateToKST = (dateString: string) =>
     new Date(dateString).toLocaleDateString('ko-KR', { timeZone: 'Asia/Seoul' });
+
   return (
-    <section className="bg-white flex flex-col">
-      <ul>
-        {qna?.length > 0 ? (
-          qna.map((qna: any) => (
-            <li key={qna.id}>
-              <h2 className="font-medium text-xl">{qna.title}</h2>
-              <p>{formatDateToKST(qna.created_at)}</p>
-              <p className="text-base">{qna.message}</p>
-            </li>
-          ))
-        ) : (
-          <div className="flex flex-col justify-center items-center">
-            <NoQnAIcon />
-            <p>문의한 내역이 없습니다.</p>
-          </div>
-        )}
-      </ul>
+    <section className="flex flex-col items-center bg-white h-[500px] gap-5">
+      {qna?.length > 0 ? (
+        <div className="w-full max-w-lg p-5 rounded-lg shadow-md border bg-[#151515] border-black h-[480px] overflow-y-auto [&::-webkit-scrollbar]:hidden">
+          <h2 className="text-2xl font-bold text-white mb-4">My QnA</h2>
+          <ul className="space-y-4">
+            {qna.map((item: any) => (
+              <li key={item.id} className="border p-4 rounded-lg bg-white shadow">
+                <h2 className="font-medium text-xl">{item.title}</h2>
+                <p className="text-sm text-gray-600">{formatDateToKST(item.created_at)}</p>
+                <p className="text-base">{item.message}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center">
+          <NoQnAIcon />
+          <p>문의한 내역이 없습니다.</p>
+        </div>
+      )}
     </section>
   );
 };
