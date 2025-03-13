5d7b7203899436121610e76fc3da4fbf1a368464
wooseok <youkn0wthat@naver.com>
2025-03-14 01:05:42 +0900

    📝 마이페이지  - 리뷰  로직

--- src/app/mypage/_components/MypageReview.tsx
+++ src/app/mypage/_components/MypageReview.tsx
@@ -1,4 +1,5 @@
 import { useReviews } from '@/hooks/useReviews';
+import { ReviewsType } from '@/types/review.type';
 import LikeIcon from '@/ui/icon/LikeIcon';
 import NoReviewIcon from '@/ui/icon/NoReviewIcon';
 import React from 'react';
@@ -10,23 +11,32 @@ const MypageReview = () => {
     new Date(dateString).toLocaleDateString('ko-KR', { timeZone: 'Asia/Seoul' });
 
   return (
-    <section className="p-4">
-      <ul>
-        {reviews?.length > 0 ? (
-          reviews.map((review: any) => (
-            <li key={review.id} className="border-b-2 border-b-black">
-              <p>{formatDateToKST(review.created_at)}</p>
-              <p>{review.comment}</p>
-              <LikeIcon />
-            </li>
-          ))
-        ) : (
-          <div className="bg-white flex flex-col justify-center items-center gap-3 py-5">
-            <NoReviewIcon />
-            <p>작성한 리뷰가 존재하지 않습니다.</p>
-          </div>
-        )}
-      </ul>
+    <section className="flex flex-col items-center bg-white h-[500px] gap-5">
+      {reviews?.length > 0 ? (
+        <div className="w-full max-w-lg p-5 rounded-lg shadow-md border bg-[#151515] border-black h-[480px] overflow-y-auto [&::-webkit-scrollbar]:hidden">
+          <h2 className="text-2xl font-bold text-white mb-4">My Reviews</h2>
+          <ul className="space-y-4">
+            {reviews.map((review: ReviewsType) => (
+              <li key={review.id} className="border p-4 rounded-lg bg-white shadow">
+                <div className="flex justify-between">
+                  <p>{review.theaters?.name}</p>
+                  <p className="text-sm text-gray-600">{formatDateToKST(review.created_at)}</p>
+                </div>
+                <p className="text-base font-semibold">{review.comment}</p>
+                <div className="flex items-center gap-2 text-gray-600">
+                  <LikeIcon />
+                  <span>{review.like_count}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center">
+          <NoReviewIcon />
+          <p>작성한 리뷰가 존재하지 않습니다.</p>
+        </div>
+      )}
     </section>
   );
 };
