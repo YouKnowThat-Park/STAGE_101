265c1f3650723e3aa375172a8493b93bdbcf8eea
wooseok <youkn0wthat@naver.com>
2025-03-20 05:45:27 +0900

    📝 qr코드 이미지 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
       'zxryfhnxexxyxmrctocl.supabase.co', // ✅ Supabase 이미지 도메인
       'k.kakaocdn.net', // ✅ 카카오 프로필 이미지 도메인 추가
       'lh3.googleusercontent.com', // ✅ Google 프로필 이미지 도메인 추가
+      'api.qrserver.com',
     ],
   },
 };
