055d4a9c5d356c84704f235f86df56ea137cc93a
wooseok <youkn0wthat@naver.com>
2025-03-21 17:46:21 +0900

    📝 Home 화면 임시 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
 import Link from 'next/link';
+import HomeQna from './home/HomeQna';
+import HomeReviews from './home/HomeReviews';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <div className="w-full h-[400px] bg-white shadow-md rounded-lg p-6">
+    <div className="min-h-screen flex flex-col items-center">
+      {' '}
+      {/* 전체 중앙 정렬 */}
+      <div className="w-full max-w-[1100px] h-[400px] bg-white shadow-md rounded-lg p-6">
         <p>Welcome to Home!</p>
       </div>
-      <div className="flex gap-10">
-        <Link href={'/reviews'}>
-          <div className=" h-[400px] bg-white shadow-md rounded-lg mt-20">
-            <p>방명록 남기러 가기</p>
-          </div>
-        </Link>
+      <div className="flex justify-center gap-10 mt-20 w-full max-w-[1100px]">
+        {/* 리뷰 전체보기 */}
         <div>
-          <p>안녕하신가</p>
+          <div className="h-[400px] bg-white shadow-md rounded-lg flex flex-col items-center ">
+            <Link href={'/reviews'} className="flex-1">
+              <p className="ml-[330px] text-sm border-b  border-black">리뷰 전체 보기</p>
+            </Link>
+            <HomeReviews />
+          </div>
+        </div>
+
+        {/* QnA 섹션 */}
+        <div className="h-[400px] bg-white shadow-md rounded-lg flex-1 flex flex-col items-center ">
+          <Link href={'/home/HomeQna'}>
+            <h2 className="w-full text-center mt-3">전체 보기</h2>
+          </Link>
+          <HomeQna />
         </div>
       </div>
     </div>
