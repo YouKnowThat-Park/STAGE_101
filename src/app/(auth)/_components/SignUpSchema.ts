4467c61ae4f0bcf8c58fc7c5ebb0aa08d29a4f15
wooseok <youkn0wthat@naver.com>
2025-03-23 02:12:16 +0900

    🍻 📝 회원가입시 입력 누락 방지를 위해 .nonempty 적용

--- src/app/(auth)/_components/SignUpSchema.ts
+++ src/app/(auth)/_components/SignUpSchema.ts
@@ -5,19 +5,34 @@ export const signupSchema = emailPasswordSchema
   .extend({
     name: z
       .string()
+      .nonempty({ message: '이름을 입력해주세요.' })
       .min(2, { message: '이름은 최소 2자 이상이어야 합니다.' })
       .trim()
-      .regex(/^[A-Za-z가-힣]+$/, { message: '이름에는 공백,숫자를 포함할 수 없습니다.' }),
-    nickname: z.string().min(2, { message: '닉네임은 최소 2자 이상이어야 합니다.' }).trim(),
+      .regex(/^[A-Za-z가-힣]+$/, {
+        message: '이름에는 공백, 숫자를 포함할 수 없습니다.',
+      }),
+
+    nickname: z
+      .string()
+      .nonempty({ message: '닉네임을 입력해주세요.' })
+      .min(2, { message: '닉네임은 최소 2자 이상이어야 합니다.' })
+      .trim(),
+
     phone: z
       .string()
-      .length(11, '휴대폰 번호를 정확히 입력해주세요.')
-      .regex(/^\d+$/, '숫자만 입력해주세요.'),
+      .nonempty({ message: '휴대폰 번호를 입력해주세요.' })
+      .length(11, { message: '휴대폰 번호는 11자리여야 합니다.' })
+      .regex(/^\d+$/, { message: '숫자만 입력해주세요.' }),
+
     birthdate: z
       .string()
-      .length(6, '생년월일은 6자리YYMMDD)여야 합니다.')
-      .regex(/^\d{6}$/, '생년월일은 숫자 6자리(YYMMDD) 형식이어야 합니다.'),
-    confirmPassword: z.string(),
+      .nonempty({ message: '생년월일을 입력해주세요.' })
+      .length(6, { message: '생년월일은 6자리(YYMMDD)여야 합니다.' })
+      .regex(/^\d{6}$/, {
+        message: '생년월일은 숫자 6자리(YYMMDD) 형식이어야 합니다.',
+      }),
+
+    confirmPassword: z.string().nonempty({ message: '비밀번호를 다시 입력해주세요.' }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: '비밀번호가 일치하지 않습니다.',
