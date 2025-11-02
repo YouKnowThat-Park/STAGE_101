72db02ed43b82ed0c2a6c687e432785f8b6045d5
wooseok <youkn0wthat@naver.com>
2025-11-03 03:31:41 +0900

    feat: Shop 상세 페이지 GET API 추가

--- server/routes/shop.py
+++ server/routes/shop.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from server.database import get_db
 from server.models.shop import Shop
 from server.schemas.shop import ShopResponse
+from uuid import UUID
 from typing import List
 
 router = APIRouter(prefix="/shops", tags=["Shop"])
@@ -11,3 +12,10 @@ router = APIRouter(prefix="/shops", tags=["Shop"])
 def get_shops(db: Session = Depends(get_db)):
     shops = db.query(Shop).all()
     return shops
+
+@router.get("/{shop_id}", response_model=ShopResponse)
+def get_shop_by_id(shop_id:UUID, db: Session = Depends(get_db)):
+    shop = db.query(Shop).filter(Shop.id == shop_id).first()
+    if not shop:
+        raise HTTPException(status_code=404, detail="Shop not found")
+    return shop 
