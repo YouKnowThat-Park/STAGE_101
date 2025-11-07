d1036d79147129c6a20dde814d7f3ce36fb9dc9f
wooseok <youkn0wthat@naver.com>
2025-11-07 23:55:22 +0900

    refactor: 함수 문법 변경 (화살표 함수 사용)

--- web/src/lib/api/shop/shop.ts
+++ web/src/lib/api/shop/shop.ts
@@ -1,17 +1,17 @@
 const BASE_URL = 'http://localhost:8000';
 
-export async function fetchShops() {
+export const fetchShops = async () => {
   const res = await fetch(`${BASE_URL}/shops/`, {
     method: 'GET',
   });
   if (!res.ok) throw new Error('shop 데이터를 불러오지 못했습니다.');
   return res.json();
-}
+};
 
-export async function fetchShopById(id: string) {
+export const fetchShopById = async (id: string) => {
   const res = await fetch(`${BASE_URL}/shops/${id}`, {
     method: 'GET',
   });
   if (!res.ok) throw new Error('해당 shop 데이터를 찾을 수 없습니다.');
   return res.json();
-}
+};
