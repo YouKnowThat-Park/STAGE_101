787578137b15a3cf05958d8afe51f9a2c1479163
wooseok <youkn0wthat@naver.com>
2025-11-22 03:54:00 +0900

    refactor: API 함수 폴더 위치 변경 (cart → cart_history)

--- web/src/hooks/cart_history/usePostCartHistory.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-import { createCartHistory } from 'src/lib/api/cart_history/createCartHistory';
-
-export const usePostCartHistory = () => {
-  return useMutation({
-    mutationFn: createCartHistory,
-  });
-};
