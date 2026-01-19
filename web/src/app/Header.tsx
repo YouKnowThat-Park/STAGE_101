55be57ee6a432f71cd57422695477161d2a3cc08
wooseok <youkn0wthat@naver.com>
2026-01-19 21:36:35 +0900

    style: inner 값 수정

--- web/src/app/Header.tsx
+++ web/src/app/Header.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Logout from './(auth)/_components/Logout';
 import { usePathname } from 'next/navigation';
 import { HeaderProps } from 'src/types/common/common-type';
+const INNER = 'max-w-[1280px] mx-auto px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12';
 
 const Header = ({ user }: HeaderProps) => {
   const pathname = usePathname();
@@ -12,7 +13,7 @@ const Header = ({ user }: HeaderProps) => {
 
   return (
     <header>
-      <div className="w-full h-16 bg-white mb-[50px] mt-10 ">
+      <div className={`${INNER}w-full h-16 mb-[20px] mt-10 `}>
         <Logout user={user} />
       </div>
     </header>
