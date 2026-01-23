abdcfbb2a2dc1073e5aab8375a69745298990e9e
wooseok <youkn0wthat@naver.com>
2026-01-24 03:02:14 +0900

    refactor: 그래프 랭킹 폴더 구조 변경

--- web/src/app/home/graph-ranking/ReviewRankingModal.tsx
+++ web/src/app/home/graph-ranking/ReviewRankingModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import HomeReviews from '../home/HomeReviews';
+import HomeReviews from '../HomeReviews';
 
 const ReviewRankingModal = ({ onClose }: { onClose: () => void }) => {
   return (
