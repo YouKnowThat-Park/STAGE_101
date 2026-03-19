a22720f7394ba70b6cbcafb22fed341d21cb6dee
wooseok <youkn0wthat@naver.com>
2026-03-19 21:36:32 +0900

    perf: Suspense fallback 로딩 UI로 인해 발생하던 CLS 해결

--- web/src/app/loading.tsx
+++ web/src/app/loading.tsx
@@ -1,25 +1,20 @@
 'use client';
+
 import Image from 'next/image';
-import React, { useEffect } from 'react';
 
 const Loading = () => {
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    return () => {
-      document.body.style.overflow = '';
-    };
-  }, []);
-
   return (
-    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/60">
-      <Image
-        src="/stage101_loader.webp"
-        alt="로딩 중"
-        width={300}
-        height={300}
-        className="animate-pulse"
-        priority
-      />
+    <div className="relative min-h-[calc(100vh-160px)] w-full">
+      <div className="absolute inset-0 flex items-center justify-center bg-black/60">
+        <Image
+          src="/stage101_loader.webp"
+          alt="로딩 중"
+          width={300}
+          height={300}
+          className="animate-pulse"
+          priority
+        />
+      </div>
     </div>
   );
 };
